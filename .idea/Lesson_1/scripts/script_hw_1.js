5dd392d30da2850b42d9627e4fc3df04cceeea40
famillion <slapfama@gmail.com>
2020-04-04 21:20:55 +0300

    Lesson 1

--- .idea/Lesson_1/scripts/script_hw_1.js
+++ .idea/Lesson_1/scripts/script_hw_1.js
@@ -1,9 +1,16 @@
 
 let money = prompt('Ваш бюджет на месяц?','')
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', ''),
+    rashod = prompt('Введите обязательную статью расходов в этом месяце',''),
+    summ = prompt('Во сколько обойдется?','');
 
-appData = {
-    appMoney: money,
-    timeData: time,
+    let appData = {
+        appMoney: money,
+        timeData: time,
+        expenses: { rashod, summ },
+        optionalExpenses: {undefined},
+        income: [undefined],
+        savings: false
+    };
 
-}
+    alert(money / 30);
